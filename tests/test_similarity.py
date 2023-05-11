6b3b690a3e8dc2a2cc5293c1df295f2e9bdca250
Carlos Alberto Costa Beppler <beppler@gmail.com>
2023-05-11 10:01:37 -0300

    Add unit tests.

--- tests/test_similarity.py
+++ tests/test_similarity.py
@@ -1,8 +1,8 @@
 from html_similarity import style_similarity
 from html_similarity.style_similarity import jaccard_similarity
+from html_similarity import structural_similarity
 
 from .utils import almost_equal
-
 
 html1 = ''''
 <html>
@@ -44,3 +44,36 @@ def test_jaccard_similarity():
     assert almost_equal(0.6666, jaccard_similarity(['a', 'b'], ['a', 'b', 'c']))
     assert 0 == jaccard_similarity(['d', 'e'], ['a', 'b', 'c'])
     assert almost_equal(jaccard_similarity(list(range(1, 1000000)), list(range(1000000 - 10, 2 * 1000000))), 0)
+
+xhtml_1 = '''
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+    <body class="body-class">
+        <h1 class="title">This a title<h1>
+        <ul>
+            <li class="item active">item 1</li>
+            <li class="item">item 2</li>
+            <li class="item">item 3</li>
+        <ul>
+    </body>
+</html>
+'''
+
+xhtml_2 = '''
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+    <body class="body-class">
+        <h1 class="title">This a different title<h1>
+        <ul>
+            <li class="item active">item 1</li>
+            <li class="item">item 2</li>
+            <li class="item">item 3</li>
+        <ul>
+    </body>
+</html>
+'''
+
+def test_bytes_similarity():
+    assert 1 == structural_similarity(xhtml_1.encode('utf-8'), xhtml_2.encode('utf-8'))
